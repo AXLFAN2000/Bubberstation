6ca08c5fe6f051490e036a336ba3efa2f29f07d3
JOEL <gildenflame@gmail.com>
2024-12-30 23:45:34 -0500

    help me god help me

--- modular_skyrat/master_files/code/modules/client/preferences/laugh.dm
+++ modular_skyrat/master_files/code/modules/client/preferences/laugh.dm
@@ -2,6 +2,8 @@
 	category = PREFERENCE_CATEGORY_NON_CONTEXTUAL
 	savefile_identifier = PREFERENCE_CHARACTER
 	savefile_key = "character_laugh"
+	COOLDOWN_DECLARE(laugh_test_cooldown)
+
 
 /datum/preference/choiced/laugh/init_possible_values()
 	return assoc_to_keys(GLOB.laugh_types)
@@ -11,3 +13,19 @@
 	if(laugh_id)
 		var/datum/laugh_type/laugh_type = new laugh_id
 		target.selected_laugh = laugh_type
+
+/datum/preference_middleware/laugh/proc/play_laugh(list/params, mob/user)
+	if(!COOLDOWN_FINISHED(src, laugh_test_cooldown))
+		return TRUE
+	if(user.gender == MALE || !LAZYLEN(H.selected_laugh.female_laughsounds))
+		return pick(H.selected_laugh.male_laughsounds)
+	else
+		return pick(H.selected_laugh.female_laughsounds)
+
+
+
+
+	SEND_SOUND(user, preferences.read_preference(/datum/preference/choiced/laugh)
+
+	COOLDOWN_START(src, laugh_test_cooldown, 0.5 SECONDS)
+	return TRUE
--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/character_laugh.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/character_laugh.tsx
@@ -1,8 +1,35 @@
 // THIS IS A SKYRAT UI FILE
-import { FeatureChoiced } from '../../base';
+import { Button, Stack } from '../../../../../../components';
+import {
+  FeatureChoiced,
+  FeatureChoicedServerData,
+  FeatureValueProps,
+} from '../../base';
 import { FeatureDropdownInput } from '../../dropdowns';
+
+const FeatureLaughDropdownInput = (
+  props: FeatureValueProps<string, string, FeatureChoicedServerData>,
+) => {
+  return (
+    <Stack>
+      <Stack.Item grow>
+        <FeatureDropdownInput {...props} />
+      </Stack.Item>
+      <Stack.Item>
+        <Button
+          onClick={() => {
+            props.act('play_laugh');
+          }}
+          icon="play"
+          width="100%"
+          height="100%"
+        />
+      </Stack.Item>
+    </Stack>
+  );
+};
 
 export const character_laugh: FeatureChoiced = {
   name: 'Character Laugh',
-  component: FeatureDropdownInput,
+  component: FeatureLaughDropdownInput,
 };
